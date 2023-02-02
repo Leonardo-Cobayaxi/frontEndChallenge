c2732bf290650dea5410a85f7aa4fa07aa47d4d8
Leonardo <leonardokoba80@gmail.com>
2023-02-02 15:13:11 -0300

    indicador de progresso

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -2,6 +2,8 @@ import { Card } from "./Card"
 import './ProductList.css'
 import { useEffect, useState } from "react"
 import axios from "axios"
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export type Product = {
     brand: string
@@ -31,6 +33,11 @@ export const ProductList: React.FC = () => {
 
     return (
         <div className="list">
+            {!products
+                ? <Box sx={{ display: 'flex', height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center" }}>
+                    <CircularProgress />
+                </Box>
+                : null}
             {products
                 ? products.map((product) => {
                     return <Card {...product} />
